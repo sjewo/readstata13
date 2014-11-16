a8cb27a6d55cf045111f398bd5ea6c43cf9273f4
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2014-11-16 13:38:51 +0100

    fixed some compiler  warnings

--- src/rcpp_readstata.cpp
+++ src/rcpp_readstata.cpp
@@ -270,7 +270,7 @@ List stata(const char * filePath, const bool missing)
         double const dmax = 0x1.fffffffffffffp1022;
         if (fread (&erg, sizeof(double), 1, file) == 0)
           perror ("Error reading data");
-        if (missing == FALSE & ((erg<dmin) | (erg>dmax)) )
+        if ((missing == FALSE) & ((erg<dmin) | (erg>dmax)) )
           as<NumericVector>(df[i])[j] = NA_REAL;
         else
           as<NumericVector>(df[i])[j] = erg;
@@ -283,7 +283,7 @@ List stata(const char * filePath, const bool missing)
         float const minmax = 0x1.fffffp126;
         if (fread (&erg, sizeof(float), 1, file) == 0)
           perror ("Error reading data");
-        if (missing == FALSE & ((erg<(-minmax)) | (erg>minmax)) )
+        if ((missing == FALSE) & ((erg<(-minmax)) | (erg>minmax)) )
           as<NumericVector>(df[i])[j] = NA_REAL;
         else
           as<NumericVector>(df[i])[j] = erg;
@@ -295,7 +295,7 @@ List stata(const char * filePath, const bool missing)
         int32_t erg;
         if (fread (&erg, sizeof(signed int), 1, file) == 0)
           perror ("Error reading data");
-        if (missing == FALSE & ((erg<(-2147483647)) | (erg>2147483620)) )
+        if ((missing == FALSE) & ((erg<(-2147483647)) | (erg>2147483620)) )
           as<IntegerVector>(df[i])[j] = NA_REAL;
         else
           as<IntegerVector>(df[i])[j] = erg;
@@ -307,7 +307,7 @@ List stata(const char * filePath, const bool missing)
         int16_t erg;
         if (fread (&erg, sizeof(short int), 1, file) == 0)
           perror ("Error reading data");
-        if (missing == FALSE & ((erg<(-32767)) | (erg>32740)) )
+        if ((missing == FALSE) & ((erg<(-32767)) | (erg>32740)) )
           as<IntegerVector>(df[i])[j] = NA_REAL;
         else
           as<IntegerVector>(df[i])[j] = erg;
@@ -319,7 +319,7 @@ List stata(const char * filePath, const bool missing)
         char erg;
         if (fread (&erg, sizeof(char), 1, file) == 0)
           perror ("Error reading data");
-        if (missing == FALSE & ( (erg<(-127)) | (erg>100)) )
+        if ((missing == FALSE) & ( (erg<(-127)) | (erg>100)) )
           as<IntegerVector>(df[i])[j] = NA_REAL;
         else
           as<IntegerVector>(df[i])[j] = erg;

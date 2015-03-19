4e52cbed8bad9f9bdbc6e7a46713997efec8e6fc
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2015-03-19 23:24:19 +0100

    Updated RcppExports.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,31 +9,23 @@ using namespace Rcpp;
 List stata(const char * filePath, const bool missing);
 RcppExport SEXP readstata13_stata(SEXP filePathSEXP, SEXP missingSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const char * >::type filePath(filePathSEXP );
-        Rcpp::traits::input_parameter< const bool >::type missing(missingSEXP );
-        List __result = stata(filePath, missing);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const char * >::type filePath(filePathSEXP);
+    Rcpp::traits::input_parameter< const bool >::type missing(missingSEXP);
+    __result = Rcpp::wrap(stata(filePath, missing));
+    return __result;
 END_RCPP
 }
 // stataWrite
 int stataWrite(const char * filePath, Rcpp::DataFrame dat);
 RcppExport SEXP readstata13_stataWrite(SEXP filePathSEXP, SEXP datSEXP) {
 BEGIN_RCPP
-    SEXP __sexp_result;
-    {
-        Rcpp::RNGScope __rngScope;
-        Rcpp::traits::input_parameter< const char * >::type filePath(filePathSEXP );
-        Rcpp::traits::input_parameter< Rcpp::DataFrame >::type dat(datSEXP );
-        int __result = stataWrite(filePath, dat);
-        PROTECT(__sexp_result = Rcpp::wrap(__result));
-    }
-    UNPROTECT(1);
-    return __sexp_result;
+    Rcpp::RObject __result;
+    Rcpp::RNGScope __rngScope;
+    Rcpp::traits::input_parameter< const char * >::type filePath(filePathSEXP);
+    Rcpp::traits::input_parameter< Rcpp::DataFrame >::type dat(datSEXP);
+    __result = Rcpp::wrap(stataWrite(filePath, dat));
+    return __result;
 END_RCPP
 }

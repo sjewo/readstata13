cca43409b2a42c6a93aca94590bb6d67bbf5ffae
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2016-07-12 00:57:48 +0200

    generate.factors nonint.factors

--- tests/testthat/test_read.R
+++ tests/testthat/test_read.R
@@ -22,7 +22,6 @@ missings <- system.file("extdata", "missings.dta", package="readstata13")
 
 
 dd118 <- read.dta13(missings, missing.type = FALSE)
-
 dd118_m <- read.dta13(missings, missing.type = TRUE)
 
 mvals <- attr(dd118_m, "missing")$missings
@@ -31,6 +30,44 @@ test_that("missings", {
   expect_true(datacompare(dd, dd118))
   expect_true(datacompare(dd, dd118_m))
   expect_identical(mvals, as.numeric(0:26))
+})
+
+
+# rm(list = files)
+
+#### generate factors TRUE ####
+
+dd <- data.frame(v1 = as.numeric(1:2))
+dd$v1 <- factor(x = dd$v1, levels = 1:2, labels = c("one", "2"))
+
+gen_fac <- system.file("extdata", "gen_fac.dta", package="readstata13")
+
+
+
+dd118 <- read.dta13(gen_fac, convert.factors = TRUE, generate.factors = TRUE)
+
+test_that("generate.factors TRUE", {
+  expect_true(datacompare(dd, dd118))
+})
+
+
+# rm(list = files)
+
+
+#### noint.factors TRUE ####
+
+dd <- data.frame(v1 = as.numeric(1:2))
+dd$v1 <- factor(x = dd$v1, levels = 1:2, labels = c("one", "1.2"))
+
+nonint <- system.file("extdata", "nonint.dta", package="readstata13")
+
+
+
+dd118 <- read.dta13(nonint, convert.factors = TRUE, generate.factors = TRUE,
+                    nonint.factors = TRUE)
+
+test_that("nonint.factors TRUE", {
+  expect_true(datacompare(dd, dd118))
 })
 
 
